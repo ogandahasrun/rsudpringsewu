6ab7049467f9754123f3fe170a3ed6ac0df13b43
ogandahasrun <112628704+ogandahasrun@users.noreply.github.com>
2025-03-27 12:21:35 +0700

    Add files via upload

--- table_body_gabungan.php
+++ table_body_gabungan.php
@@ -4,24 +4,24 @@
                         <td><?php echo htmlspecialchars($row['nama_brng']); ?></td>
                         <td><?php echo htmlspecialchars($row['jumlah']); ?></td>
                         <td><?php echo htmlspecialchars($row['satuan']); ?></td>
-                        <td class="currency">Rp <?php echo number_format($row['harga'], 2, ',', '.'); ?></td>
-                        <td class="currency">Rp <?php echo number_format($row['total'], 2, ',', '.'); ?></td>
+                        <td class="currency"><?php echo number_format(ceil($row['harga'])); ?></td>
+                        <td class="currency"><?php echo number_format(ceil($row['total'])); ?></td>
                     </tr>
                 <?php } ?>
                 <tr>
-                    <td colspan="5" class="currency"><strong>Total:</strong></td>
-                    <td class="currency"><strong>Rp <?php echo number_format($total_summary, 2, ',', '.'); ?></strong></td>
+                    <td colspan="5" class="currency"><strong>Total</strong></td>
+                    <td class="currency"><strong>Rp <?php echo number_format(ceil($total_summary)); ?></strong></td>
                 </tr>
                 <tr>
                     <td colspan="5" class="currency"><strong>PPN 11%:</strong></td>
-                    <td class="currency"><strong>Rp <?php echo number_format($ppn, 2, ',', '.'); ?></strong></td>
+                    <td class="currency"><strong>Rp <?php echo number_format(ceil($ppn)); ?></strong></td>
                 </tr>
                 <tr>
-                    <td colspan="5" class="currency"><strong>Total dengan PPN:</strong></td>
-                    <td class="currency"><strong>Rp <?php echo number_format($total_with_ppn, 2, ',', '.'); ?></strong></td>
+                    <td colspan="5" class="currency"><strong>Total dengan PPN</strong></td>
+                    <td class="currency"><strong>Rp <?php echo number_format(ceil($total_with_ppn)); ?></strong></td>
                 </tr>
                 <tr>
-                    <td colspan="6" class="currency"><strong>Terbilang:</strong> <?php echo $terbilang; ?></td>
+                    <td colspan="6" class="currency"><strong>Terbilang</strong> <?php echo $terbilang; ?></td>
                 </tr>
                 <?php } else { ?>
                 <tr>
