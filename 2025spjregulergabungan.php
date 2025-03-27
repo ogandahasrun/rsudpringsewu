a306e63c16784309a99c2e72316095f5ffa2fdaa
ogandahasrun <112628704+ogandahasrun@users.noreply.github.com>
2025-03-27 13:24:16 +0700

    Add files via upload

--- 2025spjregulergabungan.php
+++ 2025spjregulergabungan.php
@@ -96,8 +96,8 @@
                     <th>Nama Barang</th>
                     <th>Jumlah</th>
                     <th>Satuan</th>
-                    <th class="currency">Harga</th>
-                    <th class="currency">Total</th>
+                    <th class="currency" style="text-align: center;">Harga</th>
+                    <th class="currency" style="text-align: center;">Total</th>
                 </tr>
             </thead>
             <tbody>
@@ -131,3 +131,82 @@
     </div>
 </body>
 </html>
+
+<!------------------------------ BATAS HALAMAN 1  ------------------------------->
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SPJ Halaman 2</title>
+    <link rel="stylesheet" href="style.css">
+    
+</head>
+<body>
+    <div class="container">
+        <!-- Halaman Kedua -->
+        <div class="page-break">
+            <!-- Panggil file header.php -->
+            <?php include 'header.php'; ?>
+
+            <table class="no-border-table">
+                <tr><td>Ditujukan kepada Yth</td><td>:</td><td>Pejabat Pengadaan Obat/BMHP E-Katalog/Non Katalog</td></tr>
+                <tr><td>Dari</td><td>:</td><td>Pejabat Pembuat Komitmen</td></tr>
+                <tr><td>Tanggal</td><td>:</td><td>.........</td></tr>
+                <tr><td>Nomor</td><td>:</td><td>445 /<?php echo isset($pemesanan['no_order']) ? $pemesanan['no_order'] : ''; ?>.01/ PPBJ / LL.04 /...../ 2025</td></tr>
+                <tr><td>Perihal</td><td>:</td><td>Pengadaan Langsung</td></tr>    
+            </table>
+
+            <table class="no-border-table">
+                <tr><td>Dengan hormat,</td></tr>    
+            </table>
+            <table class="no-border-table">
+                <tr><td>A.</td><td>Dasar</td></tr>
+                <tr><td> </td><td>1. Peraturan Presiden Nomor 12 Tahun 2021 tentang Pengadaan Barang/Jasa Pemerintah</td></tr>
+                <tr><td> </td><td>2. Peraturan Bupati Nomor 17 Tahun 2018 tentang Jenjang Nilai Pengadaan Barang dan Jasa pada Unit Pelayanan Umum Daerah </td></tr>
+                <tr><td> </td><td>&nbsp;&nbsp;&nbsp;&nbsp;Rumah Sakit Umum Daerah Pringsewu.</td></tr>
+                <tr><td> </td><td>3. Surat permintaan Pengadaan Barang/Jasa Nomor : 445/<?php echo isset($pemesanan['no_order']) ? $pemesanan['no_order'] : ''; ?>.01/LL.04/2025 tanggal ... </td></tr>
+            </table>
+            <table class="no-border-table">
+                <tr><td>B.</td><td>Menugaskan</td><td>:</td><td></td></tr>
+                <tr><td> </td><td>Nama</td><td>:</td><td>Wisnetty, S.Si., Apt., M. Kes</td></tr>
+                <tr><td> </td><td>NIP</td><td>:</td><td>19701020 200002 2002</td></tr>
+                <tr><td> </td><td>Jabatan</td><td>:</td><td>Pejabat Pengadaan Barang dan Jasa</td></tr>
+            </table>
+
+            <table class="no-border-table">
+                <tr><td>Untuk melaksanakan kegiatan berikut dengan metode Pengadaan e-Purchasing, Spesifikasi Teknis dan HPS sebagai berikut :</td></tr>    
+            </table>
+
+            <!-- Tabel Detail Barang -->
+            <table border="1" cellpadding="5" cellspacing="0">
+            <thead>
+                <tr>
+                    <th>Nomor</th>
+                    <th>Nama Barang</th>
+                    <th>Jumlah</th>
+                    <th>Satuan</th>
+                    <th class="currency" style="text-align: center;">Harga</th>
+                    <th class="currency" style="text-align: center;">Total</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php include 'table_body_gabungan.php'; ?>
+            </tbody>
+        </table>
+
+            <!-- Tanda Tangan -->
+            <div class="signature" style="text-align: center;">
+                <p>Pejabat Pembuat Komitmen</p>                
+                <br>
+                <br>
+                <p><strong><u>dr. ANDI ARMAN, Sp.PD</u></strong></p>
+                <p>NIP. 19780801 200501 1 009</p>
+            </div>   
+        </div>
+    </div>
+</body>
+</html>
+
+<!------------------------------ BATAS HALAMAN 2  ------------------------------->
--- table_body_gabungan.php
+++ table_body_gabungan.php
@@ -1,30 +1,30 @@
 <?php if (!empty($data)) { $no = 1; foreach ($data as $row) { ?>
-                    <tr>
-                        <td><?php echo $no++; ?></td>
-                        <td><?php echo htmlspecialchars($row['nama_brng']); ?></td>
-                        <td><?php echo htmlspecialchars($row['jumlah']); ?></td>
-                        <td><?php echo htmlspecialchars($row['satuan']); ?></td>
-                        <td class="currency"><?php echo number_format(ceil($row['harga'])); ?></td>
-                        <td class="currency"><?php echo number_format(ceil($row['total'])); ?></td>
-                    </tr>
-                <?php } ?>
-                <tr>
-                    <td colspan="5" class="currency"><strong>Total</strong></td>
-                    <td class="currency"><strong>Rp <?php echo number_format(ceil($total_summary)); ?></strong></td>
-                </tr>
-                <tr>
-                    <td colspan="5" class="currency"><strong>PPN 11%:</strong></td>
-                    <td class="currency"><strong>Rp <?php echo number_format(ceil($ppn)); ?></strong></td>
-                </tr>
-                <tr>
-                    <td colspan="5" class="currency"><strong>Total dengan PPN</strong></td>
-                    <td class="currency"><strong>Rp <?php echo number_format(ceil($total_with_ppn)); ?></strong></td>
-                </tr>
-                <tr>
-                    <td colspan="6" class="currency"><strong>Terbilang</strong> <?php echo $terbilang; ?></td>
-                </tr>
-                <?php } else { ?>
-                <tr>
-                    <td colspan="6" style="text-align:center;">Data tidak ditemukan</td>
-                </tr>
-                <?php } ?>
+    <tr>
+            <td><?php echo $no++; ?></td>
+            <td><?php echo htmlspecialchars($row['nama_brng']); ?></td>
+            <td><?php echo htmlspecialchars($row['jumlah']); ?></td>
+            <td><?php echo htmlspecialchars($row['satuan']); ?></td>
+            <td class="currency"><?php echo number_format(ceil($row['harga'])); ?></td>
+            <td class="currency"><?php echo number_format(ceil($row['total'])); ?></td>
+        </tr>
+    <?php } ?>
+    <tr>
+        <td colspan="5" class="currency"><strong>Total</strong></td>
+        <td class="currency"><strong>Rp <?php echo number_format(ceil($total_summary)); ?></strong></td>
+    </tr>
+    <tr>
+        <td colspan="5" class="currency"><strong>PPN 11%:</strong></td>
+        <td class="currency"><strong>Rp <?php echo number_format(ceil($ppn)); ?></strong></td>
+    </tr>
+    <tr>
+        <td colspan="5" class="currency"><strong>Total dengan PPN</strong></td>
+        <td class="currency"><strong>Rp <?php echo number_format(ceil($total_with_ppn)); ?></strong></td>
+    </tr>
+    <tr>
+        <td colspan="6" class="currency"><strong>Terbilang : </strong> <?php echo $terbilang; ?></td>
+    </tr>
+    <?php } else { ?>
+    <tr>
+        <td colspan="6" style="text-align:center;">Data tidak ditemukan</td>
+    </tr>
+<?php } ?>
