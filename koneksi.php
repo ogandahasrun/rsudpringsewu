d9747b675bd5398c96b6cb991ca4986de0dd4daf
ogandahasrun <hasrunoganda@gmail.com>
2025-08-13 09:55:51 +0700

    Exclude koneksi.php

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$koneksi = mysqli_connect("LOCALHOST","backup","backup","sik");
-// Check connection
-if (mysqli_connect_errno()){
-	echo "Koneksi database gagal : " . mysqli_connect_error();
-}else{
-echo "";
-}
-?>
