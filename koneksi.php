6ac972e462378cecf093f39439795d9d9f73c8c6
ogandahasrun <hasrunoganda@gmail.com>
2025-09-30 11:42:23 +0700

    Ignore koneksi.php

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$koneksi = mysqli_connect("192.168.1.88","backup","backup","sik");
-// Check connection
-if (mysqli_connect_errno()){
-	echo "Koneksi database gagal : " . mysqli_connect_error();
-}else{
-echo "";
-}
-?>
